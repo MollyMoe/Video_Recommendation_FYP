a06efabdc6a1ee743a472e032da5bb930b253d8c
ClaraXinn <claraxin8@gmail.com>
2025-05-26 19:46:15 +0800

    add server

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB error:', err));
+
+  app.use('/api/auth', authRoutes);
+ // adjust path if needed
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
