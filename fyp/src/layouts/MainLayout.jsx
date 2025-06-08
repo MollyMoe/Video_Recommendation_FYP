7b458c945fb7db0aedeb739e9cd27f05b8fded9f
MollyMoe <mcmyamya@gmail.com>
2025-06-08 18:39:29 +0800

    name changes

--- fyp/src/layouts/MainLayout.jsx
+++ fyp/src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Nav from '../components/Nav';
-import SideBar from '../components/SideBar';
+import Nav from '../streamer_components/Nav';
+import SideBar from '../streamer_components/SideBar';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
