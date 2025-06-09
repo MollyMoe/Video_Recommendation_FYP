ad054f91b207afafdbfe058bcbbf97a3864ddfd3
MollyMoe <mcmyamya@gmail.com>
2025-06-09 15:43:52 +0800

    name changes for user roles

--- fyp/src/components/streamer_components/StSideBar.jsx
+++ fyp/src/components/streamer_components/StSideBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom';
-import SidebarButton from './SidebarButton';
+import SidebarButton from './StSidebarButton';
 
 const SideBar = () => {
   const location = useLocation();
