fee261ff5ada0babb8e03f3993dd7afe63d1e843
MollyMoe <mcmyamya@gmail.com>
2025-06-11 16:01:33 +0800

    Update AdminLayout.jsx

--- fyp/src/layouts/AdminLayout.jsx
+++ fyp/src/layouts/AdminLayout.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import Nav from '../components/streamer_components/StNav';
-import SideBar from '../components/streamer_components/StSideBar';
 import { Outlet } from 'react-router-dom';
-import AdDashboard from '../components/admin_components/AdDashboard';
 import AdNav from '../components/admin_components/AdNav';
 
 const AdminLayout = () => {
