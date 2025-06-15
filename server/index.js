869720954bd73355bd5b9c7ce956b94ecc6ccaec
Uf4i <ufairahbafana12@gmail.com>
2025-06-15 21:34:39 +0800

    home content data is added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/auth');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -11,15 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Primary connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB error:', err));
+})
+.then(() => console.log("Main DB connected"))
+.catch((err) => console.error(" Main DB error:", err));
 
-  app.use('/api/auth', authRoutes);
- // adjust path if needed
-
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/movies", require("./routes/movieRoutes"));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on http://localhost:${process.env.PORT}`);
---
+++ server/models/Movie.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const MovieSchema = new mongoose.Schema({
+  userId: String,
+  movieId: String,
+  predicted_rating: Number,
+  title: String,
+  genres: [String],
+  tmdb_id: String,
+  poster_url: String,
+  trailer_key: String,
+  trailer_url: String,
+}, { timestamps: true });
+
+
+module.exports = (conn) => conn.model('Movie', MovieSchema, 'hybridRecommendation');
