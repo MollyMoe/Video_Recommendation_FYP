f7231a032d6652e0cdebcc1cbd83244195fb6a73
Mya Hmue <myahmue80@gmail.com>
2025-06-16 15:29:06 +0800

    modal chnaged

--- server/models/Streamer.js
+++ server/models/Streamer.js
@@ -8,9 +8,9 @@ const StreamerSchema = new mongoose.Schema({
   email: String,
   fullName: String,
   userType: { type: String, default: 'streamer' },
-  status: { type: String, default: 'Active' }, 
-  /*resetToken: String,
-  tokenExpiry: Date,*/
+  status: { type: String, default: 'Active' } ,
+  resetToken: String,
+  tokenExpiry: Date,
 });
 
 
