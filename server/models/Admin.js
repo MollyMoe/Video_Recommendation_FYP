fcc7f4eb8629ba7143ff7b70ceff4fb4d57b84e2
ClaraXinn <claraxin8@gmail.com>
2025-06-16 13:59:13 +0800

    adminID added

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Counter = require('./Counter'); 
 
 const AdminSchema = new mongoose.Schema({
+  userId: { type: String, unique: true },
   username: String,
   password: String,
   email: String,
@@ -10,13 +11,13 @@ const AdminSchema = new mongoose.Schema({
 });
 
 AdminSchema.pre("save", async function (next) {
-  if (!this.shortId) {
+  if (!this.userId) {
     const counter = await Counter.findByIdAndUpdate(
-      { _id: "adminId" }, // 🔑 Unique counter for admin
+      { _id: "adminId" }, 
       { $inc: { sequence_value: 1 } },
       { new: true, upsert: true }
     );
-    this.shortId = "A" + counter.sequence_value.toString().padStart(3, "0"); // like A001, A002
+    this.userId = "A" + counter.sequence_value.toString().padStart(3, "0"); 
   }
   next();
 });
