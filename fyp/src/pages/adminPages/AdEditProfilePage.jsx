552a9b927dde61de0f2ab4135c9152aedb34ac75
Mya Hmue <myahmue80@gmail.com>
2025-06-16 20:13:53 +0800

    Added change password

--- fyp/src/pages/adminPages/AdEditProfilePage.jsx
+++ fyp/src/pages/adminPages/AdEditProfilePage.jsx
@@ -239,7 +239,6 @@ const AdEditProfilePage = () => {
           </div>
 
           <div className="mb-5">
-<<<<<<< HEAD
             <button
               type="button"
               onClick={() => setShowPasswordModal(true)}
@@ -247,18 +246,6 @@ const AdEditProfilePage = () => {
             >
               Change Password
             </button>
-=======
-            <label className="block mb-2 text-sm font-medium">Change password</label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
-              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 
-              dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-            />
->>>>>>> origin/main
           </div>
 
           <div className="flex flex-col items-end space-y-2 mt-4">
