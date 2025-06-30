2dea7442fcdefa79681c4bcf0f5251149857cae2
ClaraXinn <claraxin8@gmail.com>
2025-06-30 17:47:11 +0800

    fetching latest .exe code

--- fyp/src/components/admin_components/AdNav.jsx
+++ fyp/src/components/admin_components/AdNav.jsx
@@ -1,24 +1,19 @@
-
-import userProfile from "../../images/User-profile.png";
-import { useUser } from "../../context/UserContext";
 import React, { useEffect, useState } from "react";
-import { FaMoon, FaSignOutAlt, FaUserEdit } from "react-icons/fa";
+import { useUser } from "../../context/UserContext";
+import { FaMoon, FaSignOutAlt, FaUserEdit, FaSearch } from "react-icons/fa";
 import logoPic from "../../images/Cine-It.png";
-import { FaSearch } from "react-icons/fa";
 import AdUserProfile from "./AdUserProfile";
 
 const AdNav = () => {
   const [open, setOpen] = useState(false);
-
-  const { setCurrentRole } = useUser(); // <-- Add this line
+  const { setCurrentRole } = useUser();
 
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (user?.userType) {
-      setCurrentRole(user.userType); // either 'admin' or 'streamer'
+    const savedUser = JSON.parse(localStorage.getItem("user"));
+    if (savedUser?.userType) {
+      setCurrentRole(savedUser.userType); // 'admin' or 'streamer'
     }
-  }, []);
-
+  }, [setCurrentRole]);
 
   return (
     <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
@@ -29,11 +24,11 @@ const AdNav = () => {
             <img
               className="w-33 h-15 rounded-full"
               src={logoPic}
-              alt="Cine-It.png"
+              alt="Cine-It Logo"
             />
           </div>
 
-          {/* User Profile */}
+          {/* Profile Dropdown */}
           <AdUserProfile />
         </div>
       </div>
