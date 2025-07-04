c650013afdbf09981280d1d206172fe27c05002f
MollyMoe <mcmyamya@gmail.com>
2025-07-04 17:04:09 +0800

    origins edited

--- backend/main.py
+++ backend/main.py
@@ -64,7 +64,7 @@ origins = [
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
