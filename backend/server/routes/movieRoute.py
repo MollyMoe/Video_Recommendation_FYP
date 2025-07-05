e26665618f449ddb31bb91578e5188ce2fff0147
Mya Hmue <myahmue80@gmail.com>
2025-07-06 07:27:22 +0800

    update

--- backend/server/routes/movieRoute.py
+++ backend/server/routes/movieRoute.py
@@ -5,7 +5,6 @@ from fastapi.responses import JSONResponse
 from bson import ObjectId, errors
 from pydantic import BaseModel
 from typing import Optional, List, Union
-from motor.motor_asyncio import AsyncIOMotorClient
 
 class Movie(BaseModel):
     _id: Optional[str]  # ObjectId as string
@@ -72,23 +71,21 @@ async def add_to_liked_movies(request: Request):
     return {"message": "Movie added to liked list"}
 
 @router.get("/likedMovies/{userId}")
-async def get_liked_movies(userId: str, request: Request):
-    db = request.app.state.movie_db           # Motor database
+def get_liked_movies(userId: str, request: Request):
+    db = request.app.state.movie_db
     liked_collection = db["liked"]
     movies_collection = db["movies"]
 
-    # await find_one
-    liked_doc = await liked_collection.find_one({"userId": userId})
+    liked_doc = liked_collection.find_one({"userId": userId})
     if not liked_doc or not liked_doc.get("likedMovies"):
         return {"likedMovies": []}
 
     liked_ids = [to_objectid_safe(mid) for mid in liked_doc["likedMovies"]]
-    
-    # await .to_list()
-    movies = await movies_collection.find(
+
+    movies = list(movies_collection.find(
         {"_id": {"$in": liked_ids}},
         {"_id": 1, "poster_url": 1, "title": 1}
-    ).to_list(length=None)
+    ))
 
     for m in movies:
         m["_id"] = str(m["_id"])
