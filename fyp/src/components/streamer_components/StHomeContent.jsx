d5f87fe5e9bca14a0541992439efe69e28de1657
Uf4i <ufairahbafana12@gmail.com>
2025-07-06 04:53:10 +0800

    fixing regenerate button #6 (not enough data?)

--- fyp/src/components/streamer_components/StHomeContent.jsx
+++ fyp/src/components/streamer_components/StHomeContent.jsx
@@ -134,6 +134,7 @@ const handleRegenerate = async () => {
   }
 };
 
+
   if (isLoading) {
     return (
       <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
