494d6f850e56374b81649f83615c379b92ba512c
Mya Hmue <myahmue80@gmail.com>
2025-08-08 01:09:07 +0800

    testt

--- fyp/src/config/api.js
+++ fyp/src/config/api.js
@@ -1,5 +1,5 @@
-export function getAPI() {
-  return navigator.onLine
-    ? "https://cineit.onrender.com"
-    : "http://localhost:8000";
-}
+const API = navigator.onLine
+  ? "https://cineit.onrender.com"
+  : "http://localhost:8000";
+
+export { API };
